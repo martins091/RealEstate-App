215214afe38955f073b416bf55ac6921a1b3011a
martins091 <agbomartinsejiofor@gmail.com>
2024-03-01 11:19:06 +0100

    complete sign up page functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,39 +1,86 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 export default function SignUp() {
+  const [formData, setFormData] = useState({});
+  const [ error, setError ] = useState(null);
+  const [ loading, setLoading ] = useState(false);
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+  
+
+  const handleSubmit = async (e) => {
+    try {
+      setLoading(true)
+      e.preventDefault();
+      const res = await fetch('/api/auth/signup',
+        {method: 'POST',
+        headers: {
+          'content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+  
+        }
+     );
+     const data = await res.json();
+     if (data.success === false) {
+      setError(data.message);
+      setLoading(false);
+      return;
+     }
+     setLoading(false);
+     setError(null)
+     navigate('/sign-in');
+    } catch(error) {
+      setLoading(false);
+      setError(error.message)
+    }
+  }
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
-      <form className="flex flex-col gap-4">
+      <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
         <input
           type="text"
           placeholder="username"
           className="border p-3 rounded-lg"
           id="username"
+          onChange={handleChange}
         />
         <input
           type="email"
           placeholder="email"
           className="border p-3 rounded-lg"
           id="email"
+          onChange={handleChange}
         />
         <input
           type="password"
           placeholder="password"
           className="border p-3 rounded-lg"
           id="password"
+          onChange={handleChange}
         />
-        <button className="bg-slate-700 text-white rounded-lg p-3 
-            uppercase hover:opacity-95 disabled:opacity-80">
-          Sign Up
+        <button
+          className="bg-slate-700 text-white rounded-lg p-3 
+            uppercase hover:opacity-95 disabled:opacity-80"
+            disabled={loading}
+        >
+          {loading ? "Loading..." : "Sign-up"}
         </button>
-        <div className="flex gap-2 mt-5">
+      </form>
+      <div className="flex gap-2 mt-5">
           <p>Have an account? </p>
           <Link to={"/sign-in"}>
-              <span className="text-blue-700">Sign in</span>
+            <span className="text-blue-700">Sign in</span>
           </Link>
         </div>
-      </form>
+        {error && <p className="text-red-500">{error}</p> }
     </div>
   );
 }
