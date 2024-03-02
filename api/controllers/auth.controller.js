166009b5d497ba2506b7f0b09bbc13c1fdfe89c9
martins091 <agbomartinsejiofor@gmail.com>
2024-03-02 16:35:10 +0100

    add signout functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -61,3 +61,12 @@ export const google = async (req, res, next) => {
     next(error);
   }
 };
+
+export const signOut = async (req, res) => {
+  try {
+    res.clearCookie("access_token");
+    res.status(200).json('User has been logged out');
+  } catch (error) {
+    next(error)
+  }
+}
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -14,6 +14,9 @@ import {
   deleteUserFailure,
   deleteUserStart,
   deleteUserSuccess,
+  signOutUserStart,
+  signOutUserFailure,
+  signOutUserSuccess,
 } from "../redux/user/userSlice";
 import { useDispatch } from "react-redux";
 export default function Profile() {
@@ -100,6 +103,21 @@ export default function Profile() {
     }
   };
 
+  const handleSignOut = async() => {
+    try {
+       dispatch(signOutUserStart())
+      const res = await fetch('/api/auth/signout');
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(signOutUserFailure(data.message))
+        return;
+      }
+      dispatch(signOutUserSuccess(data.message))
+    } catch (error){
+      dispatch(signOutUserFailure(error.message))
+    }
+  }
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
@@ -163,8 +181,9 @@ export default function Profile() {
         <span onClick={handleDeleteUser} className="text-red-700">
           Delete Account
         </span>
-        <span className="text-red-700">Sign Out</span>
+        <span onClick={handleSignOut} className="text-red-700 cursor-pointer">Sign Out</span>
       </div>
+      <p className="text-red-700 mt-5">{error ? error : ""}</p>
       <p className="text-gray-700 mt-5">
         {updateSuccess ? "User is updated successfully" : ""}
       </p>
