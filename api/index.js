c1322925b0a330bc76e91f47bfd68fda1e6dc752
martins091 <agbomartinsejiofor@gmail.com>
2024-03-11 15:29:48 +0100

    fixed some backend bug

--- api/index.js
+++ api/index.js
@@ -36,9 +36,10 @@ app.use("/api/listing", listingRouter)
 
 app.use(express.static(path.join(__dirname, '/client/dist')));
 
-app.length('*', (req, res) => {
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-})
+});
+
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || 'Internal Server Error'
